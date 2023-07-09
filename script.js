5d94f9767350de7205a54dfccfb8659136707d11
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-07-09 17:50:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function decimalToBinary(num) {
   // Write your code here
+	let binStr = "";
+	while(num !== 0){
+		let rem = num%2;
+		binStr+=rem;
+		num = Math.floor(num/2);
+	}
+	return binStr;
 }
 
 window.decimalToBinary = decimalToBinary;
